7212b14315781bbf41475ab2c9f8ae29f154a71b
Hossam bassem <hossambassem33@gmail.com>
2023-05-07 18:33:27 +0300

    add featured listView item

--- lib/Features/Home/presentation/views/widgets/custom_appbar.dart
+++ lib/Features/Home/presentation/views/widgets/custom_appbar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 45),
+      padding: const EdgeInsets.only(left: 24, right: 24, top: 45, bottom: 20),
       child: Row(
         children: [
           Image.asset(
--- lib/Features/Home/presentation/views/widgets/homeview_body.dart
+++ lib/Features/Home/presentation/views/widgets/homeview_body.dart
@@ -13,10 +13,27 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        CustomAppBar(),
-        CustomListViewItem(),
-      ],
+      children: const [CustomAppBar(), FeaturedBoxListView()],
+    );
+  }
+}
+
+class FeaturedBoxListView extends StatelessWidget {
+  const FeaturedBoxListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: FeaturedListViewItem(),
+          );
+        },
+      ),
     );
   }
 }
