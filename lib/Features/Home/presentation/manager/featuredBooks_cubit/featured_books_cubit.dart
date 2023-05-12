0f73f58896dc80d8de58d014bfa3bb3f8db16c1c
Hossam bassem <hossambassem33@gmail.com>
2023-05-12 18:16:05 +0300

    create featured books cubit state

---
+++ lib/Features/Home/presentation/manager/featuredBooks_cubit/featured_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Home/data/model/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
