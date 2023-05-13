63abcfd9683c4668a312e9702978f786019488bb
Hossam bassem <hossambassem33@gmail.com>
2023-05-13 14:24:02 +0300

    add git it to app

--- lib/Features/Home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/Home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,6 @@
-import 'dart:ffi';
-
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/Home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/Features/Home/data/model/book_model/repos/home_repo.dart';
-import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
